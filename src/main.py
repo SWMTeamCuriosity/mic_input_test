427f9e4b9f682ea80b51723990903e0c54542491
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-08-22 02:54:42 +0900

    init : branch init

--- src/main.py
+++ src/main.py
@@ -23,16 +23,7 @@ frames = []
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
+    print(data)
 
 print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
